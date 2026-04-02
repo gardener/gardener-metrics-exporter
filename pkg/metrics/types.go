bd605fd5e84d53dbe86d4641f23c344b9e4a8bd3
Andreea Denitiu <119592402+adenitiu@users.noreply.github.com>
2026-04-02 08:35:40 +0200

    Introduce metrics for seed reconciliation `LastOperation` state (#146)
    * add metricGardenSeedOperationState
    
    * add metricGardenSeedOperationState
    
    * export operation state metrics for the seed
    
    * fix format error
    
    * use tab indentation
    
    * run gofmt

--- pkg/metrics/types.go
+++ pkg/metrics/types.go
@@ -5,11 +5,12 @@ const (
 	metricGardenUsersSum       = "garden_users_total"
 
 	// Seed metric
-	metricGardenManagedSeedInfo = "garden_managed_seed_info"
-	metricGardenSeedInfo        = "garden_seed_info"
-	metricGardenSeedCondition   = "garden_seed_condition"
-	metricGardenSeedCapacity    = "garden_seed_capacity"
-	metricGardenSeedUsage       = "garden_seed_usage"
+	metricGardenManagedSeedInfo    = "garden_managed_seed_info"
+	metricGardenSeedInfo           = "garden_seed_info"
+	metricGardenSeedCondition      = "garden_seed_condition"
+	metricGardenSeedCapacity       = "garden_seed_capacity"
+	metricGardenSeedUsage          = "garden_seed_usage"
+	metricGardenSeedOperationState = "garden_seed_operation_states"
 
 	// Gardenlet metric
 	metricGardenGardenletCondition          = "garden_gardenlet_condition"
